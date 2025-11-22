dffad0b551c5f6db0372535f1b15dc8d4dae45bb
Юлия (Джулия) <sixquestions666@gmail.com>
2025-11-22 19:50:32 +0300

    Create forms.py

---
+++ films/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Film
+
+class FilmForm(forms.ModelForm):
+    class Meta:
+        model = Film
+        fields = ['title', 'description', 'review']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'review': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+        }
+        labels = {
+            'title': 'Название фильма',
+            'description': 'Описание',
+            'review': 'Отзыв',
+        }
